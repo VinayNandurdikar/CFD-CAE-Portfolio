a5b210671a6811c287955f5fac5665aab24c599a
VinayNandurdikar <117153025+VinayNandurdikar@users.noreply.github.com>
2026-08-24 12:41:16 +0530

    Enhance basic calculator with explanatory comments
    Added comments to explain user input and operations.

--- Scientific_Computing_Python/01_Python_Basics/01_basic_calculator.py
+++ Scientific_Computing_Python/01_Python_Basics/01_basic_calculator.py
@@ -1,1 +1,30 @@
+# input() takes information from the user.
+# input() gives the value as text, so float() converts it into a number.
+# float() is used instead of int() because it also allows decimal numbers like 10.5.
 
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# + is used for addition
+addition = num1 + num2
+
+# - is used for subtraction
+subtraction = num1 - num2
+
+# * is used for multiplication
+multiplication = num1 * num2
+
+# print() displays the result
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+
+# Division by zero is not allowed.
+# != means "not equal to".
+# Therefore, divide only when num2 is not zero.
+
+if num2 != 0:
+    division = num1 / num2
+    print("Division:", division)
+else:
+    print("Cannot divide by zero")
